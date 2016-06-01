5514f4c0bba48ca9081f58db230e03fc808e554a
Zack Forbing <zackforbing@gmail.com>
2016-05-31 19:20:13 -0600

    bullshit commit to enable pulling

--- lib/server.rb
+++ lib/server.rb
@@ -1,12 +1,5 @@
-#Move controller and connect to server.rb
-#get debugger to work with Hello world
-
-
-
-require './lib/parser'
+require_relative 'parser'
 require 'socket'
-require './lib/controller'
-
 require 'timeout'
 require 'pry'
 
@@ -14,10 +7,11 @@ require 'pry'
 
 class Server
 
-# Server
-#add loop to Server
+
   tcp_server = TCPServer.new(9292)
   client = tcp_server.accept
+
+  all_count = 0
   timeout_in_seconds = 4
 
   while tcp_server.listen(1)
@@ -28,9 +22,26 @@ class Server
         request_lines << line.chomp
       end
       p=Parser.new(request_lines)
-# end server
+
       #puts "Got this request:"
       #puts request_lines.inspect
+
+      puts "Sending response."
+      response = "<pre>#{p.hello_world}\n #{p.verb}\n#{p.path}\n#{p.protocol}\n#{p.host}\n#{p.port}\n#{p.origin}\n#{p.accept}</pre>"
+      output = "<html><head></head><body>#{response}</body></html>"
+      headers = ["http/1.1 200 ok",
+        "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+        "server: ruby",
+        "content-type: text/html; charset=iso-8859-1",
+        "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+        client.puts headers
+        client.puts output
+
+        puts ["Wrote this response:", headers, output].join("\n")
+
+        all_count += 1
+        puts "\nResponse complete, exiting."
+
       end
 
     end
