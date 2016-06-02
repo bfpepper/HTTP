a02107916cb94f36130736e53e5bda603eea42ad
Zack Forbing <zackforbing@gmail.com>
2016-06-01 19:57:51 -0600

    iteration 3 fixed. more spice added to the potential refactor. more work needed.

--- beth_help.rb
+++ beth_help.rb
@@ -28,34 +28,46 @@
 #     - word search
 #       - dictionary
 #       - word
+#below is the remains of route method, make sure to change all calls to response
+def response(request)
+  @all_count += 1
+  respond = "<pre>\n#{request.each { |k, v| puts k + ": " + v }}</pre>"
+  output = "<html><head></head><body>#{content}\n\n#{respond}</body></html>"
+  headers = ["http/1.1 200 ok",
+    "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+    "server: ruby",
+    "content-type: text/html; charset=iso-8859-1",
+    "content-length: #{output.length}\r\n\r\n"].join("\r\n")
 
-      def response(request)
-        respond = "<pre>\n#{request.each { |k, v| puts k + ": " + v }}</pre>"
-        output = "<html><head></head><body>#{content}\n\n#{respond}</body></html>"
-        headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    #below should be parser class
 
-        @all_count += 1
+    content = hello_world         if request['Path'] == "/hello"
+    content = datetime            if request['Path'] == "/datetime"
+    content = shutdown            if request['Path'] == "/shutdown"
+    content = ""                  if request['Path'] == "/"
+    content = word_search(word)   if request['Path'].include? "/word_search"
 
-        content = hello_world                     if request['Path'] == "/hello"
-        content = datetime                        if request['Path'] == "/datetime"
-        content = "Total requests: #{@all_count}" if request['Path'] == "/shutdown"
-        content = ["Wrote this response:", headers, output].join("\n") if request['Path'] == "/"
+    #below should be responder class
 
-        content = word_search(word)                if ,,,,
+    puts "Sending response."
+    puts ["Wrote this response:", headers, output].join("\n")
 
-          # puts "Sending response."
-          puts
-        elsif request['Path'].include? "/word_search"
-          word = request['Path'].partition('=').last
+    def hello_world
+      result =  "Hello, world! (#{@hello_world})"
+      @hello_world += 1
+      result
+    end
 
-        end
-        response = [headers + output]
+    def datetime
+      "date: #{Time.now.strftime('%l:%m %p on %A, %B %e, %Y')}"
+    end
+
+    def shutdown
+      "Total requests: #{@all_count}"
+    end
+
+    def word_search(word)
+      word_included = File.read("/usr/share/dict/words").split("\n").include?(word)
+      word_included ? "#{word} is a known word" : "#{word} is not a known word"
       end
-
-def datetime
-  "date: #{Time.now.strftime('%l:%m %p on %A, %B %e, %Y')}"
-end
+    end
